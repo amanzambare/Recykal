60ce0c435493bcd1f4b8fd05cce45263492ed218
amanzambare <76624676+amanzambare@users.noreply.github.com>
2021-08-03 13:04:51 +0530

    second milestone

--- cart/src/Product.js
+++ cart/src/Product.js
@@ -1,42 +1,62 @@
-import React,{useState,useEffect} from 'react'
- const Product = () => {
-     const[adata,setAdata]= useState([])
+import React, { useState, useEffect } from 'react'
+const Product = () => {
+    const [adata, setAdata] = useState([]);
+    const [added, setadded] = useState([]);
+
+
 
     useEffect(() => {
 
-        const data=async()=>{
-            const url='https://gist.githubusercontent.com/sandeepdillerao/edb372a95d6cf1a2a49b79577d023281/raw/75bf5e59e47748fad0d01ca63c81dd3791c2615c/product.json'
-            const response=await fetch(url);
-            const res=await response.json();
+        const data = async () => {
+            const url = 'https://gist.githubusercontent.com/sandeepdillerao/edb372a95d6cf1a2a49b79577d023281/raw/75bf5e59e47748fad0d01ca63c81dd3791c2615c/product.json'
+            const response = await fetch(url);
+            const res = await response.json();
             setAdata(res);
 
             console.log(res);
 
         }
         data();
-    
+
     }, [])
-    return (<div className="main">
+
+
+    const addtocart = (pro) => {
+        console.log(pro);
+        setadded([...added, pro]);
+
+    }
+    // const remitm =(productss)=>{
+    //     setadded(added.filter((prod)=>prod !== productss))
+    // })
+    const remitm =(data1)=>{
+        setadded(
+            added.filter((dataa)=> dataa !==data1)
+        )
+    }
+    
+    return (
+    <div className="main">
         <div>
             <h3>brands</h3>
             <ul>
 
-            {adata.map((brands)=>{
-                return( <li>{brands.brand}</li>)
-                
-            }
-            )}
+                {adata.map((brands,ind) => {
+                    return (<li key={ind}>{brands.brand}</li>)
+
+                }
+                )}
             </ul>
         </div>
 
         <div className="cards">
             {/* <h3>Products</h3> */}
-            {adata.map((a)=>{
+            {adata.map((a, id) => {
 
-                
-                return(
-                    
-                    <div >
+
+                return (
+
+                    <div key={id}>
                         <div className="card">
 
                             <img src={a.icon} />
@@ -44,16 +64,36 @@ import React,{useState,useEffect} from 'react'
                             <h6>Price:₹{a.price}</h6>
                             <h6>Weigth{a.weight}</h6>
                             <h4>{a.brand}</h4>
-                            <button>Add to cart</button>
+                            <button onClick={() => addtocart(a)}>Add to cart</button>
                         </div>
                     </div>
-                
-                
-            )
-                       })}
-            
+
+
+                )
+            })}
+
+
         </div>
+        <div>
+        <ul>
+            cart Items:{added.length}
+            {
+                added.map((carti,indx) => {
+                    return (
+                        <div key={indx}>
+                        <img src={carti.icon} />
+                            <p>{carti.name}</p>
+                            <h6>Price:₹{carti.price}</h6>
+                            <h6>Weigth{carti.weight}</h6>
+                            <h4>{carti.brand}</h4>
+                           <button onClick={()=>remitm(carti)}>remove</button>
+                            </div>
+                    )
+                })
+            }
+        </ul>
         </div>
+    </div>
     )
 }
 export default Product;

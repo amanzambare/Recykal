94ebd20400be82dbf0bf8d23cad8eaec728c2441
amanzambare <76624676+amanzambare@users.noreply.github.com>
2021-08-03 12:01:58 +0530

    first milestone

---
+++ cart/src/App.js
@@ -0,0 +1,13 @@
+import Ract from "react";
+import Product from "./Product";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Product/>
+    </div>
+  );
+}
+
+export default App;
